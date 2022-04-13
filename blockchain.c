9310f3ae0c85da01e38fe54c4eb0ef87d23c6169
Norras <nora@LAPTOP-27S5MN57.localdomain>
2022-04-13 10:09:55 +0200

    exo 8 terminé (enfin jcrois)

--- blockchain.c
+++ blockchain.c
@@ -7,6 +7,7 @@
 #include "blockchain.h"
 #include <openssl/sha.h>
 #include "secure.h"
+#include "math.h"
 
 
 void save_block(Block *b){
@@ -99,49 +100,6 @@ int compute_proof_of_work(Block *b,int d){
     return -1;
 }
 
-// int perfs(Block *b,int d){
-//     unsigned char *hash;
-//     char nonce[10];
-//     char zeros[d+1];
-//     memset(zeros,'0',d);
-//     zeros[d]='\0';
-//     clock_t begin=time(NULL);
-//     for(int i=0;i<100000000;i++){
-//         // sprintf(nonce,"%d",i);
-//         hash=SHA256("25531",6,0);
-//         // hash[d]='\0';
-//         // if (strcmp(hash,zeros)==0){
-//         //     printf("%d -- VALIDE\n",i);
-//         //     b->nonce=i;
-//         //     return i;
-//         // }
-//     }
-//     clock_t end=time(NULL);
-
-//     printf("TEMPS CALCULÉ : %f\n",difftime(end,begin));
-//     return -1;
-// }
-
-// int compute_proof_of_work2(Block *b,int d){
-//     unsigned char *hash;
-//     char nonce[256];
-//     sprintf(nonce,"0");
-//     hash=SHA256(nonce,strlen(nonce),0);
-//     char zeros[d+1];
-//     memset(zeros,'0',d);
-//     zeros[d]='\0';
-//     hash[d]='\0';
-//     unsigned int i=0;
-//     while(strcmp((char *)hash,zeros)!=0){
-//         sprintf(nonce,"%d",i);
-//         hash=SHA256(nonce,strlen(nonce),0);
-//         hash[d]='\0';
-//         i++;
-//     }
-//     printf("%d\n",i);
-//     b->nonce=i;
-//     return i;
-// }
 int verify_block(Block *b,int d){
     char nonce[256];
     char zeros[d+1];
@@ -155,4 +113,140 @@ int verify_block(Block *b,int d){
         return 1;
     }
     return 0;
+}
+
+
+void delete_block(Block *b){
+    free(b->hash);
+    free(b->previous_hash);
+    CellProtected *tmp;
+    while(b->votes!=NULL){
+        tmp=b->votes->next;
+        free(b->votes);
+        b->votes=tmp;
+    }
+    free(b);
+}
+
+
+CellTree *create_node(Block *b){
+    CellTree *cell=(CellTree *)malloc(sizeof(CellTree));
+    cell->block=b;
+    cell->father=NULL;
+    cell->firstChild=NULL;
+    cell->nextBro=NULL;
+    cell->height=0;
+
+    return cell;
+}
+
+int update_height(CellTree *father,CellTree *child){
+    int maximum=fmax(father->height,child->height+1);
+    if (maximum==father->height){
+        return 0;
+    }
+    father->height=maximum;
+    return 1;
+}
+
+void add_child(CellTree *father,CellTree *child){
+    // Ajout en tête du fils
+    child->nextBro=father->firstChild;
+    father->firstChild=child;
+
+    // Modification de la hauteur des parents
+    CellTree *ftmp=father;
+    CellTree *ctmp=father->firstChild;
+    while (ftmp!=NULL){
+        update_height(ftmp,ctmp);
+        ftmp=ftmp->father;
+        ctmp=ctmp->father;
+    }
+}
+
+/* Fonction d'affichage d'un arbre
+--  ATTENTION : PROF DOIT ETRE APPELE AVEC LA VALEUR 0*/
+void print_tree(CellTree *racine,int prof){
+    if (racine==NULL){
+        return;
+    }
+    char tabs[racine->height];
+    CellTree *cour=racine;
+    memset(tabs,'\t',prof);
+    while (cour!=NULL){
+        printf("%sHauteur:%s Hash:%s\n",tabs,cour->block->hash);
+        print_tree(cour->firstChild,++prof);
+        cour=cour->nextBro;
+    }
+}
+
+
+void delete_node(CellTree *node){
+    delete_block(node->block);
+    free(node);
+}
+
+void delete_tree(CellTree *tree){
+    CellTree *child=tree->firstChild;
+    CellTree *tmp;
+    while (child!=NULL){
+        tmp=child->nextBro;
+        delete_tree(child);
+        child=tmp;
+    }
+    delete_node(child);
+}
+
+CellTree *highest_child(CellTree *cell){
+    if (cell->firstChild==NULL){
+        return NULL;
+    }
+    CellTree *child=cell->firstChild->nextBro;
+    CellTree *max=cell->firstChild;
+    while (child !=NULL){
+        if (max->height<child->height){
+            max=child;
+        }
+        child=child->nextBro;
+    }
+    return max;
+}
+
+CellTree *last_node(CellTree *tree){
+    if (tree->firstChild==NULL){
+        return tree;
+    }
+    return last_node(highest_child(tree));
+}
+
+void fusion_cell_protected(CellProtected **first, CellProtected **second){
+    if (first==NULL){
+        first=second;
+        return;
+    }
+    CellProtected *tmp=first;
+    while(tmp->next!=NULL){
+        tmp=tmp->next;
+    }
+    tmp->next=second;
+}
+
+
+CellProtected *fusion_highest_CP(CellTree *racine){
+    CellProtected *res=racine->block->votes;
+    CellProtected *tmp=racine->block->votes;
+    while ((tmp!=NULL) && (tmp->next!=NULL)){
+        tmp=tmp->next;
+    }
+    CellTree *cour=highest_child(racine);
+
+
+    while (cour!=NULL){
+        fusion_cell_protected(tmp,cour->block->votes);
+        cour=highest_child(racine);
+        while ((tmp!=NULL) && (tmp->next!=NULL)){
+            tmp=tmp->next;
+        }
+    }
+    return res;
 }
